9b7adffd02045f6233c4c88f31a0f352247c6483
kennethsauers <kennethsauers@gmail.com>
2019-05-28 13:45:35 -0400

    added cifar100 model

--- inception.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow import keras
-
-model = tf.keras.models.load_model('model/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5')
-
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar100.load_data(label_mode='fine')
-
-print(x_train[4])
